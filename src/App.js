67eb6093ff839944cbc111ac0d95f1f95d10de88
Mayuri Chougule <chougulemayuri98@gmail.com>
2022-08-28 17:56:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
+ 
+const App = () =>{
+  const [quoteText, setquoteText] = useState("");
+  const [quoteAuthor, setquoteAuthor] = useState("");
+  const [quoteGen, setquoteGen] = useState("");
 
-function App() {
+  const quoteAPI = async () =>{
+    let arrayOfQuotes = [];
+    try{
+      const data = await axios.get("https://quote-garden.herokuapp.com/api/v3/quotes/random");
+      arrayOfQuotes = data.data;
+    }catch(error){
+      console.log(error);
+    }
+
+    try{
+      setquoteText(arrayOfQuotes.data[0].quoteText);
+      setquoteAuthor(arrayOfQuotes.data[0].quoteAuthor);
+      setquoteGen(arrayOfQuotes.data[0].quoteGenre);
+    }catch(error){
+        console.log(error);
+    }
+  };
+
+  useEffect(() =>{
+      quoteAPI();
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="wrapper">
+        <div className="content">
+          <div className="quote-area">
+            <i className="fa fa-quote-left"></i>
+            <p className="quote">{quoteText}</p>
+            <i className="fa fa-quote-right"></i>
+          </div>
+          <div className="author">
+            <span>__</span>
+            <span className="name">{quoteAuthor}</span>
+          </div>
+          <div className="quotegen">
+            <span className="name">{quoteGen}</span>
+          </div>
+        </div>
+        <div className="buttons">
+            <button onClick={quoteAPI}>New Quote</button>
+        </div>
+      </div>
+    </>
+    
   );
 }
 
 export default App;
+
